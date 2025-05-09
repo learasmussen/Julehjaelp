4a1827fac9b2ca59e780ff10c53deeccfac3bd1f
lea03 <ler002@edu.zealand.dk>
2025-05-09 09:39:23 +0200

    test

--- index.php
+++ index.php
@@ -42,6 +42,8 @@ require "settings/init.php";
     </div>
 </div>
 
+<div></div>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 <script src="js.js"></script>
 </body>
