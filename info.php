036c02472b8a70135579fce1cbaedf8e757dca2c
lea03 <ler002@edu.zealand.dk>
2025-05-16 10:16:25 +0200

    rettet fejl

--- info.php
+++ info.php
@@ -28,9 +28,9 @@ require "settings/init.php";
 
 <div class="container-fluid px-3 px-md-0 mx-md-0 mb-5 pb-5 mt-3 mt-lg-5">
     <!-- Wrapper til de tre bokse -->
-    <div class="d-flex flex-column flex-md-row justify-content-between mb-4 px-md-3 px-lg-5">
+    <div class="d-flex flex-column flex-md-row mb-4 px-md-3 px-lg-5">
         <!-- Første grønne boks -->
-        <div class="bg-darkgreen text-white p-4 rounded-5 flex-fill me-md-3 me-lg-4">
+        <div class="col-md-6 bg-darkgreen text-white p-4 rounded-5 flex-fill mb-3 mb-md-0 mx-md-2">
             <h5 class="fw-bold info-header-text text-center pb-3 mb-0 roboto">JULEHJÆLP</h5>
             <p class="mb-0 info-middle-text py-3 instrument">Julehjælp er støtte til udsatte familier, så deres børn kan få en tryg og værdig jul.</p>
             <ul class="mb-0 info-list-items pb-3 pt-4 px-3 instrument">
@@ -46,7 +46,7 @@ require "settings/init.php";
         </div>
 
         <!-- Anden grønne boks -->
-        <div class="bg-darkgreen text-white p-4 rounded-5 flex-fill me-md-3 me-lg-4">
+        <div class="col-md-6 bg-darkgreen text-white p-4 rounded-5 flex-fill mx-md-2">
             <h5 class="fw-bold info-header-text text-center pb-3 mb-0 roboto">ØNSKETRÆET</h5>
             <p class="mb-0 info-middle-text py-3 instrument">Et digitalt juletræ med ønsker fra danske børn i krisecentre og lignende.</p>
             <ul class="mb-0 info-list-items pb-3 pt-4 instrument px-3">
@@ -54,14 +54,12 @@ require "settings/init.php";
                 <li class="pb-3">Du skal blot betale for ønsket, så klarer vi resten – super nemt og hurtigt.</li>
             </ul>
             <p class="mb-0 info-list-items py-md-2 instrument">Giv en gave og få et barns juleønske til at gå i opfyldelse.</p>
-
         </div>
-
-
     </div>
 
+
     <!-- Gul knap: Desktop -->
-    <div class="text-center d-none d-md-block mt-lg-5">
+    <div class="text-center d-none d-md-flex justify-content-center mt-lg-5">
         <a href="payment.php" class="btn btn-yellow fw-bold px-4 py-2 doner-et-beløb-knap roboto">Donér et valgfrit beløb</a>
     </div>
 </div>
