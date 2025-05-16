b9d3be11cf02a0741a5cb2e842fd0af6352e9229
Marc <mah035@edu.zealand.dk>
2025-05-16 12:34:51 +0200

    Fixed choose-gift.php AGAIN. Let's hope it stays this time.

--- choose-gift.php
+++ choose-gift.php
@@ -20,39 +20,38 @@ require "settings/init.php";
 
 <?php include("includes/navbar.php") ?>
 
-<div class="container mb-5 mt-5 px-4 px-md-0">
-    <!-- Overskrift -->
+<div class="container my-5 px-4 px-md-0">
     <h2 class="text-darkgreen fw-bold text-center mb-3">VÆLG EN GAVE</h2>
-
-    <!-- Intro tekst -->
     <p class="text-center instrument h4">
-        Tak for dit bidrag til Julehjælp.
         Vælg hvilken gave du vil give herunder.
     </p>
 </div>
 
-    <div class="container g-2 mt-5">
-        <div class="row">
-            <?php
-            $sql = "SELECT * FROM wishes ORDER BY wishId ASC LIMIT 3";
-            $wishes = $db->sql($sql);
+<div class="container g-2 mt-5 mb-5">
+    <div class="row">
+        <?php
+        $sql = "SELECT * FROM wishes ORDER BY wishId ASC LIMIT 3";
+        $wishes = $db->sql($sql);
 
-            foreach($wishes as $wish) {
-                ?>
-                <div class="col-12 col-md-4 mb-5">
-                    <div class="bg-darkgreen text-white rounded-5 p-4 d-flex flex-column">
-                        <div>
-                            <h3 class="mb-2 text-center instrument"><?php echo htmlspecialchars($wish->wishName); ?> <?php echo (int)$wish->wishAge; ?> år</h3>
-                            <h5 class="mb-4 text-center instrument">Ønsker sig: <?php echo htmlspecialchars($wish->wishWish); ?></h5>
+        foreach ($wishes as $wish): ?>
+            <div class="col-12 col-md-4 mb-5">
+                <div class="card gift-card text-white text-center h-100 border-0 rounded-5 d-flex flex-column justify-content-between">
+                    <div class="card-body d-flex flex-column justify-content-between p-4">
+                        <div class="mt-auto">
+                            <h2 class="card-title mb-2 instrument">
+                                <?= htmlspecialchars($wish->wishName) ?> <?= (int)$wish->wishAge ?> år
+                            </h2>
+                            <h4 class="card-subtitle mb-4 instrument">
+                                Ønsker sig: <?= htmlspecialchars($wish->wishWish) ?>
+                            </h4>
                         </div>
-                        <a href="#" class="btn btn-yellow fw-bold px-4 py-2 mt-auto">Vælg gave</a>
+                        <a href="/payment-gift.php" class="btn btn-yellow fw-bold px-4 py-2 mt-4 w-75 mx-auto">Vælg gave</a>
                     </div>
                 </div>
-                <?php
-            }
-            ?>
-        </div>
+            </div>
+        <?php endforeach; ?>
     </div>
+</div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
