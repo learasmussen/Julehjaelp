ed8cd4eeaba76b18c7ae3da963a7137e50fca7e9
emmaprobst <emp003@edu.zealand.dk>
2025-05-16 10:01:49 +0200

    Rettet fejl

--- index.php
+++ index.php
@@ -25,27 +25,32 @@ require "settings/init.php";
 
 <?php include("includes/navbar.php") ?>
 
+<!-- Desktop version -->
 
+<!-- Baggrund og overskrift -->
 <div class="position-relative text-darkgreen img-fluid d-none d-md-block" style="height: 650px; background-image: url('images/JHDeskBackground.png'); background-size: cover; background-position: center;">
     <div class="position-absolute top-50 start-50 translate-middle text-center">
-        <h1 class="fw-bolder text-stroke" style="font-size: 65px">FÅ ET ØNSKE TIL AT GÅ I OPFYLDELSE</h1>
+        <h1 class="fw-bold" style="font-size: 70px">FÅ ET ØNSKE TIL AT <br> GÅ I OPFYLDELSE</h1>
     </div>
 </div>
 
+<!-- Tekst og knap -->
 <div class="text-center d-none d-md-block">
-    <p class="instrument h2">Velkommen til Ønsketræet!</p>
-    <a href="choose-gift.php" class="btn btn-yellow btn-lg fw-bold">Giv en gave</a>
+    <p class="instrument h3 py-3">Velkommen til Ønsketræet!</p>
+    <a href="choose-gift.php" class="btn btn-yellow btn-lg fw-bold px-5 py-2">Giv en gave</a>
 </div>
 
+
+<!-- Mobil version -->
 
 <div class="container text-center">
-    <h1 class= "text-darkgreen text-center fw-bolder fs-1 mt-5 d-block d-md-none">ØNSKETRÆET</h1>
+    <h1 class= "text-darkgreen text-center fw-bold wishingtree-text-navbar pt-5 d-block d-md-none">ØNSKETRÆET</h1>
 </div>
 <div class="container text-center mt-4 d-block d-md-none">
-    <p>Velkommen til julehjælp!</p>
-    <p>Giv julehjælpen videre, vælg et ønske og gør julen magisk for en anden</p>
-    <img class="img-fluid" src="images/træ.png" alt="træ">
-    <a class="btn btn-yellow btn-lg mt-3 fw-bold" href="choose-gift.php">Giv en gave</a>
+    <p class="instrument h5">Velkommen til Julehjælp!</p>
+    <p class="instrument h5 pt-3">Giv julehjælpen videre, vælg et ønske og gør <br> julen magisk for en anden</p>
+    <img class="img-fluid px-3 py-4" src="images/træ.png" alt="træ">
+    <a class="btn btn-yellow btn-lg py-2 fw-bold w-50" href="choose-gift.php">Giv en gave</a>
 </div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
