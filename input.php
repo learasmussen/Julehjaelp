5d557feced2bee7a3c4c3c45391114acedc60c6f
emmaprobst <emp003@edu.zealand.dk>
2025-05-27 09:42:53 +0200

    Rettet lidt forskelligt :)

--- input.php
+++ input.php
@@ -20,37 +20,45 @@ require "settings/init.php";
 
 <div class="container mt-5 mb-5">
     <!-- Overskrift -->
-    <h2 class="text-darkgreen fw-bold text-center mb-3">Velkommen til Ønskeportalen</h2>
+    <h1 class="text-darkgreen fw-bold text-center mb-3">Velkommen til Ønskeportalen</h1>
 
     <!-- Intro tekst -->
     <p class="text-center instrument h4">
-        Venligst udfyld alle felter for at tilføje ønske til Ønskehuen.
+        Udfyld alle felter for at tilføje et ønske til Ønskehuen.
     </p>
 </div>
 
-<div class="container">
-<form action="confirm.php" method="post">
-    <!-- Navn -->
-    <div class="mb-3">
-        <label for="wishName" class="form-label">Barnets navn</label>
-        <input type="text" class="form-control" id="wishName" name="wishName" required>
-    </div>
 
-    <!-- Alder -->
-    <div class="mb-3">
-        <label for="wishAge" class="form-label">Barnets alder</label>
-        <input type="number" class="form-control" id="wishAge" name="wishAge" required>
-    </div>
 
-    <!-- Ønske -->
-    <div class="mb-3">
-        <label for="wishWish" class="form-label">Barnets ønske</label>
-        <textarea class="form-control" id="wishWish" name="wishWish" rows="4" required></textarea>
-    </div>
+<div class="container d-flex flex-column mt-5">
+    <div class="flex-grow-1 d-flex justify-content-center align-items-center">
+        <div class="col-10 col-md-6 col-lg-5">
 
-    <!-- Submit knap -->
-    <div class="text-center">
-        <button type="submit" class="btn btn-darkgreen">Tilføj ønske</button>
+            <form action="confirm.php" method="post">
+                <!-- Navn -->
+                <div class="mb-3">
+                    <label for="wishName" class="form-label">Barnets Navn:</label>
+                    <input type="text" class="form-control border-black" id="wishName" name="wishName" required>
+                </div>
+
+                <!-- Alder -->
+                <div class="mb-3">
+                    <label for="wishAge" class="form-label">Barnets Alder:</label>
+                    <input type="number" class="form-control border-black" id="wishAge" name="wishAge" required>
+                </div>
+
+                <!-- Ønske -->
+                <div class="mb-3">
+                    <label for="wishWish" class="form-label">Barnets Ønske:</label>
+                    <textarea class="form-control border-black" id="wishWish" name="wishWish" rows="4" required></textarea>
+                </div>
+
+                <!-- Submit knap -->
+                <div class="text-center pt-2">
+                    <button type="submit" class="btn btn-yellow px-5">TILFØJ ØNSKE</button>
+                </div>
+            </form>
+
+        </div>
     </div>
-</form>
 </div>
--- login.php
+++ login.php
@@ -6,7 +6,7 @@ require "settings/init.php";
 <head>
     <meta charset="utf-8">
 
-    <title>Ønskeportalen login</title>
+    <title>Ønskeportalen Login</title>
 
     <meta name="robots" content="All">
     <meta name="author" content="Udgiver">
@@ -20,11 +20,11 @@ require "settings/init.php";
 
 <div class="container mt-5 mb-5">
     <!-- Overskrift -->
-    <h2 class="text-darkgreen fw-bold text-center mb-3">Velkommen til Ønskeportalen</h2>
+    <h1 class="text-darkgreen fw-bold text-center mb-3">Velkommen til Ønskeportalen</h1>
 
     <!-- Intro tekst -->
     <p class="text-center instrument h4">
-        Venligst log ind for at tilføje ønsker.
+        Log ind for at tilføje et ønske.
     </p>
 </div>
 
@@ -32,16 +32,16 @@ require "settings/init.php";
     <div class="flex-grow-1 d-flex justify-content-center align-items-center">
         <div class="login-container text-center col-10 col-md-6 col-lg-3">
             <div class="input-group mb-3">
-                <span class="input-group-text border-orange bg-white"><i class="fa-solid fa-user text-orange"></i></span>
-                <input type="text" class="form-control border-orange text-primary" placeholder="USERNAME">
+                <span class="input-group-text border-black bg-white"><i class="fa-solid fa-user"></i></span>
+                <input type="text" class="form-control border-black" placeholder="BRUGERNAVN">
             </div>
             <div class="input-group">
-                <span class="input-group-text border-orange bg-white"><i class="fa-solid fa-lock text-orange"></i></span>
-                <input type="password" class="form-control border-orange text-primary" placeholder="PASSWORD">
+                <span class="input-group-text border-black bg-white"><i class="fa-solid fa-lock"></i></span>
+                <input type="password" class="form-control border-black" placeholder="ADGANGSKODE">
             </div>
-            <a href="input.php" class="btn btn-orange text-white w-100 mt-4">LOGIN</a>
+            <a href="input.php" class="btn btn-yellow w-100 mt-4">LOG IND</a>
             <div class="mt-2 text-end">
-                <a class="text-darkgreen">Forgot password?</a>
+                <a class="text-darkgreen">Glemt adgangskode?</a>
             </div>
         </div>
     </div>
